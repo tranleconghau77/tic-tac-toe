9ca6f51e53189464c91db6e3baeaaf465a422302
Hau Tran <tranleconghau772001@gmail.com>
2024-03-02 14:33:09 +0700

    init tic tac toe project

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+export default function Player({ name, symbol }) {
+
+    const [isEditing, setIsEditing] = useState(false)
+
+    const handleEditClick = () => {
+        setIsEditing(isEditing => !isEditing)
+    }
+
+    let playerName = <span className="player-name">{name}</span>
+    let btnCaption = "Edit"
+
+    if (isEditing) {
+        playerName = <input type="text" required value={name} />
+        btnCaption = "Save"
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {playerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{btnCaption}</button>
+        </li>
+    )
+}
